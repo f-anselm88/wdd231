9572e8bfc23fb1e0e9b046c1d45da171a94b71ac
Anselm Munango <amunango2004@gmail.com>
2026-09-04 05:55:49 +0200

    Fix course order in the course array for accurate representation

--- scripts/course.js
+++ scripts/course.js
@@ -5,8 +5,8 @@ const courses = [
   { subject: "WDD", number: 130, title: "Web Fundamentals", credits: 2, completed: true },
   { subject: "CSE", number: 111, title: "Programming with Functions", credits: 2, completed: true },
   { subject: "WDD", number: 131, title: "Dynamic Web Fundamentals", credits: 2, completed: false },
-  { subject: "WDD", number: 230, title: "Programming with Classes", credits: 2, completed: false },
-  { subject: "CSE", number: 231, title: "Web Frontend Development I", credits: 2, completed: false },
+  { subject: "CSE", number: 230, title: "Programming with Classes", credits: 2, completed: false },
+  { subject: "WDD", number: 231, title: "Web Frontend Development I", credits: 2, completed: false },
 ];
 
 const courseList = document.querySelector("#course-list");
